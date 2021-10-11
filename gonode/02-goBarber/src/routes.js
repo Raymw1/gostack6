f0621c3cee8110dbc8585ec4bbbadbd7ddb5516d
Raymw1 <rayan.rmw@gmail.com>
2021-10-11 19:35:28 -0300

    GoNode: GoBarber - dashboard done

--- gonode/02-goBarber/src/routes.js
+++ gonode/02-goBarber/src/routes.js
@@ -8,12 +8,10 @@ const guestMiddleware = require("./app/middlewares/guest");
 
 const UserController = require("./app/controllers/UserController");
 const SessionController = require("./app/controllers/SessionController");
+const DashboardController = require("./app/controllers/DashboardController");
+const FileController = require("./app/controllers/FileController");
 
-routes.use((req, res, next) => {
-  res.locals.flashError = req.flash("success");
-  // res.locals.flashError = req.flash("error");
-  return next();
-});
+routes.get("/files/:file", FileController.show);
 
 routes.get("/", guestMiddleware, SessionController.create);
 routes.post("/signin", SessionController.store);
@@ -25,9 +23,6 @@ routes.use("/app", authMiddleware);
 
 routes.get("/app/logout", SessionController.destroy);
 
-routes.get("/app/dashboard", (req, res) => {
-  console.log(req.session.user);
-  return res.render("dashboard");
-});
+routes.get("/app/dashboard", DashboardController.index);
 
 module.exports = routes;
