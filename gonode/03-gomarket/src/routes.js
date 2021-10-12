96c20455cd2a89efd1c464fa9b46d991451c71d9
Raymw1 <rayan.rmw@gmail.com>
2021-10-12 20:41:37 -0300

    GoNode: goMarket - merging require controllers

--- gonode/03-gomarket/src/routes.js
+++ gonode/03-gomarket/src/routes.js
@@ -3,11 +3,10 @@ const routes = express.Router();
 
 const authMiddleware = require("./app/middlewares/auth");
 
-const UserController = require("./app/controllers/UserController");
-const SessionController = require("./app/controllers/SessionController");
+const controllers = require("./app/controllers");
 
-routes.post("/users", UserController.store);
-routes.post("/sessions", SessionController.store);
+routes.post("/users", controllers.UserController.store);
+routes.post("/sessions", controllers.SessionController.store);
 
 routes.get("/test", authMiddleware, (req, res) => res.json({ ok: true }));
 
