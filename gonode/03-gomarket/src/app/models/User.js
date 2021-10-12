eb85e3c12791061701e0b04f758471cd70aa8dfe
Raymw1 <rayan.rmw@gmail.com>
2021-10-12 17:40:55 -0300

    GoNode: goMarket - creating user schema

---
+++ gonode/03-gomarket/src/app/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: true,
+    },
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
