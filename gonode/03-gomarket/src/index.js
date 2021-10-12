57a886cdcaef19d849173be54aa31b4d06c23730
Raymw1 <rayan.rmw@gmail.com>
2021-10-12 17:35:49 -0300

    GoNode: goMarket - setting database

--- gonode/03-gomarket/src/index.js
+++ gonode/03-gomarket/src/index.js
@@ -1,6 +1,6 @@
 const server = require("./server");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Go to http://localhost:${PORT}`);
 });
--- gonode/03-gomarket/src/server.js
+++ gonode/03-gomarket/src/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const databaseConfig = require("./config/database");
 
 class App {
   constructor() {
     this.express = express();
     this.isDev = process.env.NODE_ENV !== "production";
+    this.database();
     this.middlewares();
     this.routes();
+  }
+
+  database() {
+    mongoose.connect(databaseConfig.uri, {
+      useNewUrlParser: true,
+    });
   }
 
   middlewares() {
