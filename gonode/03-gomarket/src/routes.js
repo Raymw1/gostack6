07921bee92b0d8bb213a407218c03a7808b91cf8
Raymw1 <rayan.rmw@gmail.com>
2021-10-12 20:31:51 -0300

    GoNode: goMarket - authentication done

--- gonode/03-gomarket/src/routes.js
+++ gonode/03-gomarket/src/routes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const routes = express.Router();
 
+const authMiddleware = require("./app/middlewares/auth");
+
 const UserController = require("./app/controllers/UserController");
+const SessionController = require("./app/controllers/SessionController");
 
 routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
+
+routes.get("/test", authMiddleware, (req, res) => res.json({ ok: true }));
 
 module.exports = routes;
