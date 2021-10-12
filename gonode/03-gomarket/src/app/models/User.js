8bd7f084e0f86e216f2a9a6b26d32c19461b18b0
Raymw1 <rayan.rmw@gmail.com>
2021-10-12 19:23:13 -0300

    GoNode: goMarket - encrypting password function done

--- gonode/03-gomarket/src/app/models/User.js
+++ gonode/03-gomarket/src/app/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -23,5 +24,12 @@ const UserSchema = new mongoose.Schema(
     },
   }
 );
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 8);
+});
 
 module.exports = mongoose.model("User", UserSchema);
