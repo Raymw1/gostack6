0d7e5e75bbfe1ac9928eece4fd4003080910f4d2
Raymw1 <rayan.rmw@gmail.com>
2021-10-12 22:48:45 -0300

    GoNode: goMarket - queue services

--- gonode/03-gomarket/src/app/controllers/PurchaseController.js
+++ gonode/03-gomarket/src/app/controllers/PurchaseController.js
@@ -1,6 +1,7 @@
 const Ad = require("../models/Ad");
 const User = require("../models/User");
-const Mail = require("../services/Mail");
+const PurchaseMail = require("../jobs/PurchaseMail");
+const Queue = require("../services/Queue");
 
 class PurchaseController {
   async store(req, res) {
@@ -8,13 +9,7 @@ class PurchaseController {
     const purchaseAd = (await Ad.findById(ad).populate("author")).toJSON();
     if (!purchaseAd) return res.status(400).json({ error: "Ad not found!" });
     const user = (await User.findById(req.userId)).toJSON();
-    await Mail.sendMail({
-      from: `"Rayan Wilbert" <raymw@raymw.com.br>`,
-      to: purchaseAd.author.email,
-      subject: `Ordering: ${purchaseAd.title}`,
-      template: "purchase",
-      context: { user, content, ad: purchaseAd },
-    });
+    Queue.create(PurchaseMail.key, { ad: purchaseAd, user, content }).save();
     return res.status(204).send();
   }
 }
