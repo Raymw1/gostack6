eb7c5fde301988706ce0a5d725b425970c177894
Raymw1 <rayan.rmw@gmail.com>
2021-10-13 17:01:26 -0300

    GoNode: goMarket - exception handling

--- gonode/03-gomarket/src/routes.js
+++ gonode/03-gomarket/src/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { validate } = require("express-validation");
+const handle = require("express-async-handler");
 const routes = express.Router();
 
 const authMiddleware = require("./app/middlewares/auth");
@@ -10,32 +11,36 @@ const validators = require("./app/validators");
 routes.post(
   "/users",
   validate(validators.User),
-  controllers.UserController.store
+  handle(controllers.UserController.store)
 );
 routes.post(
   "/sessions",
   validate(validators.Session),
-  controllers.SessionController.store
+  handle(controllers.SessionController.store)
 );
 
 routes.use(authMiddleware);
 
 // ADS
-routes.get("/ads", controllers.AdController.index);
-routes.get("/ads/:id", controllers.AdController.show);
-routes.post("/ads", validate(validators.Ad), controllers.AdController.store);
+routes.get("/ads", handle(controllers.AdController.index));
+routes.get("/ads/:id", handle(controllers.AdController.show));
+routes.post(
+  "/ads",
+  validate(validators.Ad),
+  handle(controllers.AdController.store)
+);
 routes.put(
   "/ads/:id",
   validate(validators.Ad),
-  controllers.AdController.update
+  handle(controllers.AdController.update)
 );
-routes.delete("/ads/:id", controllers.AdController.destroy);
+routes.delete("/ads/:id", handle(controllers.AdController.destroy));
 
 // PURCHASES
 routes.post(
   "/purchases",
   validate(validators.Purchase),
-  controllers.PurchaseController.store
+  handle(controllers.PurchaseController.store)
 );
 
 module.exports = routes;
--- gonode/03-gomarket/src/server.js
+++ gonode/03-gomarket/src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const validate = require("express-validation");
 const mongoose = require("mongoose");
+const Youch = require("youch");
 const databaseConfig = require("./config/database");
 
 class App {
@@ -9,6 +11,7 @@ class App {
     this.database();
     this.middlewares();
     this.routes();
+    this.exception();
   }
 
   database() {
@@ -23,6 +26,25 @@ class App {
 
   routes() {
     this.express.use(require("./routes"));
+  }
+
+  exception() {
+    this.express.use(async (err, req, res, next) => {
+      if (err instanceof validate.ValidationError) {
+        return res.status(err.statusCode).json(err);
+      }
+
+      if (process.env.NODE_ENV !== "production") {
+        // const youch = new Youch(err, req);
+        // return res.send(await youch.toHTML());
+        const youch = new Youch(err);
+        return res.json(await youch.toJSON());
+      }
+
+      return res
+        .status(err.statusCode || 500)
+        .json({ error: "Internal Server Error!" });
+    });
   }
 }
 
