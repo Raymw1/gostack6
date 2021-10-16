4e2fe7a0bc88d732d18e2fa931be7a930c7d51cd
Raymw1 <rayan.rmw@gmail.com>
2021-10-16 18:04:44 -0300

    GoReact: children props

--- goreact/01-concepts/src/index.js
+++ goreact/01-concepts/src/index.js
@@ -3,16 +3,26 @@ import { render } from "react-dom";
 
 class Button extends Component {
   render() {
-    return <a href="#">Save</a>;
+    return (
+      <a href={this.props.link || "Link"} onClick={this.props.onClick}>
+        {this.props.children || "Save"}
+      </a>
+    );
   }
 }
 
 class App extends Component {
+  handleClcik = () => {
+    alert("Click");
+  };
+
   render() {
     return (
       <Fragment>
         <h1>Hello, World!</h1>
-        <Button />
+        <Button link="#" onClick={this.handleClcik}>
+          Test
+        </Button>
       </Fragment>
     );
   }
