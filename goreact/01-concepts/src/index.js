4200395a15816336f99c6414b42cb9e2ae9086a1
Raymw1 <rayan.rmw@gmail.com>
2021-10-16 17:56:57 -0300

    GoReact: creating react app

--- goreact/01-concepts/src/index.js
+++ goreact/01-concepts/src/index.js
@@ -1,1 +1,10 @@
-alert("Hello, World!");
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component {
+  render() {
+    return <h1>Hello, World!</h1>;
+  }
+}
+
+render(<App />, document.getElementById("app"));
