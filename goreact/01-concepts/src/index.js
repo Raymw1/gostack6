0d7099a3028dd4afb5fd2b3f09a579f42ae16439
Raymw1 <rayan.rmw@gmail.com>
2021-10-16 17:58:36 -0300

    GoReact: using components and Fragment

--- goreact/01-concepts/src/index.js
+++ goreact/01-concepts/src/index.js
@@ -1,9 +1,20 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { render } from "react-dom";
+
+class Button extends Component {
+  render() {
+    return <a href="#">Save</a>;
+  }
+}
 
 class App extends Component {
   render() {
-    return <h1>Hello, World!</h1>;
+    return (
+      <Fragment>
+        <h1>Hello, World!</h1>
+        <Button />
+      </Fragment>
+    );
   }
 }
 
