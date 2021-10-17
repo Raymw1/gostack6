c63372daadb55c80d36a6db55d9cd7f9bae431ea
Raymw1 <rayan.rmw@gmail.com>
2021-10-17 19:46:19 -0300

    GoReact: sending repositories using props

--- goreact/02-githubCompare/src/components/CompareList/index.js
+++ goreact/02-githubCompare/src/components/CompareList/index.js
@@ -1,81 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Container, Repository } from "./styles";
 
-const CompareList = () => (
+const CompareList = ({ repositories }) => (
   <Container>
-    <Repository>
-      <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/69631?v=4"
-          alt="React"
-        />
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-    <Repository>
-      <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/69631?v=4"
-          alt="React"
-        />
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-    <Repository>
-      <header>
-        <img
-          src="https://avatars3.githubusercontent.com/u/69631?v=4"
-          alt="React"
-        />
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
-      <ul>
-        <li>
-          95,019 <small>stars</small>
-        </li>
-        <li>
-          95,019 <small>forks</small>
-        </li>
-        <li>
-          95,019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+    {repositories.map((repository) => (
+      <Repository>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
+        <ul>
+          <li>
+            {repository.stargazers_count} <small>stars</small>
+          </li>
+          <li>
+            {repository.forks_count} <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count} <small>issues</small>
+          </li>
+          <li>
+            {repository.pushed_at} <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
   </Container>
 );
 
