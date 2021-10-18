0c2bb5d11c0c0ad9c1c8aedd4c43bbf921f92757
Raymw1 <rayan.rmw@gmail.com>
2021-10-18 19:07:37 -0300

    GoReact: redux - action ToDo

--- goreact/03-fluxRedux/src/TodoList.js
+++ goreact/03-fluxRedux/src/TodoList.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
 import { connect } from "react-redux";
 
-const TodoList = (props) => (
-  <ul>
-    {props.todos.map((todo) => (
-      <li key={todo.id}>{todo.text}</li>
-    ))}
-  </ul>
+const TodoList = ({ todos, addTodo }) => (
+  <Fragment>
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+    <button onClick={() => addTodo("Add Todo")}>Add Todo</button>
+  </Fragment>
 );
 
 TodoList.propTypes = {
+  addTodo: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
@@ -24,4 +28,8 @@ const mapStateToProps = (state) => ({
   todos: state.todos,
 });
 
-export default connect(mapStateToProps)(TodoList);
+const mapDispatchToProps = (dispatch) => ({
+  addTodo: (text) => dispatch({ type: "ADD_TODO", payload: { text } }),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- goreact/03-fluxRedux/src/store/reducers/todos.js
+++ goreact/03-fluxRedux/src/store/reducers/todos.js
@@ -7,5 +7,11 @@ export default function todos(state = INITIAL_STATE, action) {
   switch (action.type) {
     default:
       return state;
+
+    case "ADD_TODO":
+      return [
+        ...state,
+        { id: state[state.length - 1]?.id + 1 || 1, text: action.payload.text },
+      ];
   }
 }
