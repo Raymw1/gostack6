7a052bdab0f532bdbb7fbb9ab07b9318c658a94e
Raymw1 <rayan.rmw@gmail.com>
2021-10-18 21:38:19 -0300

    GoReact: redux - creatinf footer

---
+++ goreact/03-fluxRedux/src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { connect } from "react-redux";
+
+const Footer = ({ count }) => <p>You have {count} favorites</p>;
+
+Footer.propTypes = {
+  count: PropTypes.number.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  count: state.favorites.length,
+});
+
+export default connect(mapStateToProps)(Footer);
--- goreact/03-fluxRedux/src/routes/index.js
+++ goreact/03-fluxRedux/src/routes/index.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Footer from "../components/Footer";
 
 import Main from "../pages/main";
 
 const Routes = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Main} />
-    </Switch>
+    <Fragment>
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+      <Footer />
+    </Fragment>
   </BrowserRouter>
 );
 
