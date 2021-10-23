5d3e20215432bfb92ab7b4ced1f534a6ee8a6f4c
Raymw1 <rayan.rmw@gmail.com>
2021-10-22 22:04:14 -0300

    Bonus: PWA - application structure

--- bonus/pwaReactJs/src/App.js
+++ bonus/pwaReactJs/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Header, Repositories } from "./styles";
+
+export default class App extends Component {
+  state = {
+    newRepoInput: "",
+    repositories: [],
+  };
+
+  addRepository = async () => {
+    const { newRepoInput } = this.state;
+    if (!newRepoInput.trim()) return;
+    const { data } = await axios.get(
+      `https://api.github.com/repos/${newRepoInput}`
+    );
+    this.setState({
+      newRepoInput: "",
+      repositories: [...this.state.repositories, data],
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Header>
+          <input
+            type="text"
+            placeholder="Add repository"
+            onChange={(e) => this.setState({ newRepoInput: e.target.value })}
+            value={this.state.newRepoInput}
+          />
+          <button onClick={this.addRepository}>Add</button>
+        </Header>
+        <Repositories>
+          {this.state.repositories.map((repository) => (
+            <li key={repository.id}>
+              <img src={repository.owner.avatar_url} alt={repository.name} />
+              <div>
+                <strong>{repository.name}</strong>
+                <p>{repository.description}</p>
+                <a href={repository.html_url}>Access</a>
+              </div>
+            </li>
+          ))}
+        </Repositories>
+      </>
+    );
+  }
 }
-
-export default App;
