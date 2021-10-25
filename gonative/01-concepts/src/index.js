e9f5f33f6f2cc71ab0221cb1aaabb9aed0becc6b
Raymw1 <rayan.rmw@gmail.com>
2021-10-25 19:34:14 -0300

    GoNative: concepts - state and immutability

--- gonative/01-concepts/src/index.js
+++ gonative/01-concepts/src/index.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Button} from 'react-native';
 
 import Todo from './components/Todo';
 
 export default class App extends Component {
+  state = {
+    todos: [],
+  };
+
+  addTodo = () => {
+    const {todos} = this.state;
+    this.setState({
+      todos: [
+        ...todos,
+        {id: todos[todos.length - 1]?.id + 1 || 0, text: 'Add'},
+      ],
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Todo title="Study GoStack" />
-        <Todo />
+        {this.state.todos.map(todo => (
+          <Todo key={todo.id} title={`${todo.id}:${todo.text}`} />
+        ))}
+        <Button title="Add Todo" onPress={this.addTodo} />
       </View>
     );
   }
