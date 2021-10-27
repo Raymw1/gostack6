049dc2d01b4255c5eec22781437cf217d8ee52ac
Raymw1 <rayan.rmw@gmail.com>
2021-10-27 19:57:09 -0300

    GoNative: gitHuber - header component done

--- gonative/02-githuber/src/pages/Repositories/index.js
+++ gonative/02-githuber/src/pages/Repositories/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
+import Header from '../../components/Header';
 
 const Repositories = () => {
   return (
     <View>
-      <Text>Repositories</Text>
+      <Header title="Repositories" />
     </View>
   );
 };
