52f052471282d49a62286ae9e7a80345931105b6
Raymw1 <rayan.rmw@gmail.com>
2021-10-28 21:24:57 -0300

    GoNative: challenge 2 - initial structure

---
+++ gonative/challenges/challenge2/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.js', '.json', '.svg', '.jpg', '.png'],
+      },
+    ],
+  ],
+};
---
+++ gonative/challenges/challenge2/src/index.js
@@ -0,0 +1,13 @@
+import './config/ReactotronConfig';
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hellow</Text>
+    </View>
+  );
+};
+
+export default App;
