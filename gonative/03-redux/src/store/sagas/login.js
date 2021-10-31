c89c63d7c9fce967346f40486d96e184839932fa
Raymw1 <rayan.rmw@gmail.com>
2021-10-31 20:54:01 -0300

    GoNative: redux - navigating in Redux

--- gonative/03-redux/src/store/sagas/login.js
+++ gonative/03-redux/src/store/sagas/login.js
@@ -3,12 +3,14 @@ import {call, put} from '@redux-saga/core/effects';
 import api from 'services/api';
 
 import * as LoginActions from 'store/actions/login';
+import {navigate} from 'services/navigation';
 
 export default function* login(action) {
   try {
     const {username} = action.payload;
     yield call(api.get, `/users/${username}`);
     yield put(LoginActions.loginSuccess(username));
+    navigate('Repositories');
   } catch (error) {
     yield put(LoginActions.loginFailure());
   }
