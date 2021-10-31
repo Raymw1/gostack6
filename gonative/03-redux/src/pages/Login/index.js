657b8863035ec84e2b68a5ae205886f515c9bba9
Raymw1 <rayan.rmw@gmail.com>
2021-10-31 19:47:43 -0300

    GoNative: redux - login page done

--- gonative/03-redux/src/pages/Login/index.js
+++ gonative/03-redux/src/pages/Login/index.js
@@ -1,12 +1,36 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import api from 'services/api';
+
+import {Container, Input, Button, ButtonText, Error} from './styles';
 
 export default class Login extends Component {
+  state = {
+    username: '',
+    error: false,
+  };
+
+  handleSubmit = async () => {
+    const {username} = this.state;
+    try {
+      await api.get(`/users/${username}`);
+    } catch (error) {}
+  };
+
   render() {
+    const {username} = this.state;
     return (
-      <View>
-        <Text> Login </Text>
-      </View>
+      <Container>
+        <Input
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Input your User"
+          value={username}
+          onChangeText={text => this.setState({username: text})}
+        />
+        <Button onPress={this.handleSubmit}>
+          <ButtonText>Login</ButtonText>
+        </Button>
+      </Container>
     );
   }
 }
