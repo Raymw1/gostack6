b27acb8070944a907297e60c6c4b99d4aa79d3f4
Raymw1 <rayan.rmw@gmail.com>
2021-11-02 23:19:26 -0300

    Bonus: RNAnimations - animating header

--- bonus/rnAnimations/02-application/src/index.js
+++ bonus/rnAnimations/02-application/src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from 'react';
 
 import {
   View,
@@ -9,64 +9,66 @@ import {
   StyleSheet,
   Dimensions,
   ScrollView,
-  TouchableOpacity
-} from "react-native";
+  TouchableOpacity,
+  Animated,
+} from 'react-native';
 
-import User from "./components/User";
+import User from './components/User';
 
-const { width } = Dimensions.get("window");
+const {width} = Dimensions.get('window');
 
 export default class App extends Component {
   state = {
+    scrollOffset: new Animated.Value(0),
     userSelected: null,
     userInfoVisible: false,
     users: [
       {
         id: 1,
-        name: "Diego Fernandes",
-        description: "Head de programação!",
-        avatar: "https://avatars0.githubusercontent.com/u/2254731?s=460&v=4",
+        name: 'Diego Fernandes',
+        description: 'Head de programação!',
+        avatar: 'https://avatars0.githubusercontent.com/u/2254731?s=460&v=4',
         thumbnail:
-          "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=400&q=80",
+          'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=400&q=80',
         likes: 200,
-        color: "#57BCBC"
+        color: '#57BCBC',
       },
       {
         id: 2,
-        name: "Robson Marques",
-        description: "Head de empreendedorismo!",
-        avatar: "https://avatars2.githubusercontent.com/u/861751?s=460&v=4",
+        name: 'Robson Marques',
+        description: 'Head de empreendedorismo!',
+        avatar: 'https://avatars2.githubusercontent.com/u/861751?s=460&v=4',
         thumbnail:
-          "https://images.unsplash.com/photo-1490633874781-1c63cc424610?auto=format&fit=crop&w=400&q=80",
+          'https://images.unsplash.com/photo-1490633874781-1c63cc424610?auto=format&fit=crop&w=400&q=80',
         likes: 350,
-        color: "#E75A63"
+        color: '#E75A63',
       },
       {
         id: 3,
-        name: "Cleiton Souza",
-        description: "Head de mindset!",
-        avatar: "https://avatars0.githubusercontent.com/u/4669899?s=460&v=4",
+        name: 'Cleiton Souza',
+        description: 'Head de mindset!',
+        avatar: 'https://avatars0.githubusercontent.com/u/4669899?s=460&v=4',
         thumbnail:
-          "https://images.unsplash.com/photo-1506440905961-0ab11f2ed5bc?auto=format&fit=crop&w=400&q=80",
+          'https://images.unsplash.com/photo-1506440905961-0ab11f2ed5bc?auto=format&fit=crop&w=400&q=80',
         likes: 250,
-        color: "#2E93E5"
+        color: '#2E93E5',
       },
       {
         id: 4,
-        name: "Robson Marques",
-        description: "Head de empreendedorismo!",
-        avatar: "https://avatars2.githubusercontent.com/u/861751?s=460&v=4",
+        name: 'Robson Marques',
+        description: 'Head de empreendedorismo!',
+        avatar: 'https://avatars2.githubusercontent.com/u/861751?s=460&v=4',
         thumbnail:
-          "https://images.unsplash.com/photo-1490633874781-1c63cc424610?auto=format&fit=crop&w=400&q=80",
+          'https://images.unsplash.com/photo-1490633874781-1c63cc424610?auto=format&fit=crop&w=400&q=80',
         likes: 350,
-        color: "#E75A63"
-      }
-    ]
+        color: '#E75A63',
+      },
+    ],
   };
 
   selectUser = user => {
-    this.setState({ userSelected: user });
-    this.setState({ userInfoVisible: true });
+    this.setState({userSelected: user});
+    this.setState({userInfoVisible: true});
   };
 
   renderDetail = () => (
@@ -77,7 +79,15 @@ export default class App extends Component {
 
   renderList = () => (
     <View style={styles.container}>
-      <ScrollView>
+      <ScrollView
+        scrollEventThrottle={16}
+        onScroll={Animated.event([
+          {
+            nativeEvent: {
+              contentOffset: {y: this.state.scrollOffset},
+            },
+          },
+        ])}>
         {this.state.users.map(user => (
           <User
             key={user.id}
@@ -90,22 +100,42 @@ export default class App extends Component {
   );
 
   render() {
-    const { userSelected } = this.state;
+    const {userSelected} = this.state;
 
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
 
-        <View style={styles.header}>
+        <Animated.View
+          style={[
+            styles.header,
+            {
+              height: this.state.scrollOffset.interpolate({
+                inputRange: [0, 140],
+                outputRange: [200, 70],
+                extrapolate: 'clamp',
+              }),
+            },
+          ]}>
           <Image
             style={styles.headerImage}
-            source={userSelected ? { uri: userSelected.thumbnail } : null}
+            source={userSelected ? {uri: userSelected.thumbnail} : null}
           />
 
-          <Text style={styles.headerText}>
-            {userSelected ? userSelected.name : "GoNative"}
-          </Text>
-        </View>
+          <Animated.Text
+            style={[
+              styles.headerText,
+              {
+                fontSize: this.state.scrollOffset.interpolate({
+                  inputRange: [0, 120, 140],
+                  outputRange: [24, 24, 18],
+                  extrapolate: 'clamp',
+                }),
+              },
+            ]}>
+            {userSelected ? userSelected.name : 'GoNative'}
+          </Animated.Text>
+        </Animated.View>
         {this.state.userInfoVisible ? this.renderDetail() : this.renderList()}
       </View>
     );
@@ -114,27 +144,25 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
 
   header: {
-    paddingTop: Platform.OS === "ios" ? 40 : 20,
+    paddingTop: Platform.OS === 'ios' ? 40 : 20,
     paddingHorizontal: 15,
-    backgroundColor: "#2E93E5",
-    height: 200
+    backgroundColor: '#2E93E5',
   },
 
   headerImage: {
-    ...StyleSheet.absoluteFillObject
+    ...StyleSheet.absoluteFillObject,
   },
 
   headerText: {
-    fontSize: 24,
-    fontWeight: "900",
-    color: "#FFF",
-    backgroundColor: "transparent",
-    position: "absolute",
+    fontWeight: '900',
+    color: '#FFF',
+    backgroundColor: 'transparent',
+    position: 'absolute',
     left: 15,
-    bottom: 20
-  }
+    bottom: 20,
+  },
 });
