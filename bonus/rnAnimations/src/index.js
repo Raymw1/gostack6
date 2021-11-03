72c533d761f17fb051b933890c6e7d27a651dce2
Raymw1 <rayan.rmw@gmail.com>
2021-11-02 22:16:11 -0300

    Bonus: RNAnimations - panResponder

--- bonus/rnAnimations/src/index.js
+++ bonus/rnAnimations/src/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, Animated} from 'react-native';
+import {Text, View, Animated, PanResponder} from 'react-native';
 
 // Animated.View, Animated.Text, Animated.Image, Animated.ScrollView
 
@@ -14,6 +14,7 @@ export default class App extends Component {
   state = {
     ballY,
     ballX,
+    ball: new Animated.ValueXY({x: 0, y: 0}),
   };
 
   componentDidMount() {
@@ -23,7 +24,7 @@ export default class App extends Component {
     // this.sequenceAnimation();
     // this.parallelAnimation();
     // this.staggerAnimation();
-    this.loopAnimation();
+    // this.loopAnimation();
   }
 
   timingAnimation = () => {
@@ -132,20 +133,55 @@ export default class App extends Component {
     ).start();
   };
 
+  componentWillMount() {
+    this._panResponder = PanResponder.create({
+      onMoveShouldSetPanResponder: (e, gestureState) => true,
+      onPanResponderGrant: (e, gestureState) => {
+        this.state.ball.setOffset({
+          x: this.state.ball.x._value,
+          y: this.state.ball.y._value,
+        });
+        this.state.ball.setValue({x: 0, y: 0});
+      },
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          {
+            dx: this.state.ball.x,
+            dy: this.state.ball.y,
+          },
+        ],
+        {
+          listener: (e, gestureState) => {
+            console.log(gestureState);
+          },
+        },
+      ),
+      onPanResponderRelease: () => {
+        this.state.ball.flattenOffset();
+      },
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Animated.View
+          {...this._panResponder.panHandlers}
           style={[
             styles.ball,
             {
-              top: this.state.ballY,
-              left: this.state.ballX,
-              opacity: this.state.ballY.interpolate({
-                inputRange: [0, 100, 200],
-                outputRange: [1, 1, 0.2],
-                extrapolate: 'clamp',
-              }),
+              // top: this.state.ballY,
+              // left: this.state.ballX,
+              // opacity: this.state.ballY.interpolate({
+              //   inputRange: [0, 100, 200],
+              //   outputRange: [1, 1, 0.2],
+              //   extrapolate: 'clamp', // STOP
+              // }),
+              transform: [
+                {translateX: this.state.ball.x},
+                {translateY: this.state.ball.y},
+              ],
             },
           ]}
         />
