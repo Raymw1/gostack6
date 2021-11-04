03c99c525437a2766b7d1d7ffe36c6bd6b49bc45
Raymw1 <rayan.rmw@gmail.com>
2021-11-03 22:51:03 -0300

    GoNode: adonisJS - store User

--- gonode/04-adonisjs/app/Models/User.js
+++ gonode/04-adonisjs/app/Models/User.js
@@ -9,11 +9,6 @@ const Hash = use('Hash')
 class User extends Model {
   static boot() {
     super.boot()
-
-    /**
-     * A hook to hash the user password before saving
-     * it to the database.
-     */
     this.addHook('beforeSave', async (userInstance) => {
       if (userInstance.dirty.password) {
         userInstance.password = await Hash.make(userInstance.password)
@@ -21,16 +16,6 @@ class User extends Model {
     })
   }
 
-  /**
-   * A relationship on tokens is required for auth to
-   * work. Since features like `refreshTokens` or
-   * `rememberToken` will be saved inside the
-   * tokens table.
-   *
-   * @method tokens
-   *
-   * @return {Object}
-   */
   tokens() {
     return this.hasMany('App/Models/Token')
   }
