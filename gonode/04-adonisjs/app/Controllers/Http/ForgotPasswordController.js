9fdf101b64e064ccced498e5c75224b8fbb28885
Raymw1 <rayan.rmw@gmail.com>
2021-11-04 20:51:07 -0300

    GoNode: adonisJS - forgot password: email sending

--- gonode/04-adonisjs/app/Controllers/Http/ForgotPasswordController.js
+++ gonode/04-adonisjs/app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store({ request, response }) {
@@ -11,6 +12,20 @@ class ForgotPasswordController {
       user.token = crypto.randomBytes(10).toString('hex')
       user.token_created_at = new Date()
       await user.save()
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        (message) => {
+          message
+            .to(email)
+            .from('rayan@gonode.com', 'Rayan | GoNode')
+            .subject('Password Recovery')
+        }
+      )
     } catch (err) {
       return response.status(err.status).send({
         error: {
