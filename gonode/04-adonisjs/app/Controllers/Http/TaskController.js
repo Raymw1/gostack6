006328a3b609bc8d6ebbd2c81b1c8707a83aee2d
Raymw1 <rayan.rmw@gmail.com>
2021-11-05 16:11:48 -0300

    GoNode: adonisJS - tasks CRUD done

--- gonode/04-adonisjs/app/Controllers/Http/TaskController.js
+++ gonode/04-adonisjs/app/Controllers/Http/TaskController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Task = use('App/Models/Task')
+
 /**
  * Resourceful controller for interacting with tasks
  */
@@ -11,82 +13,65 @@ class TaskController {
   /**
    * Show a list of all tasks.
    * GET tasks
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new task.
-   * GET tasks/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index({ params }) {
+    const tasks = await Task.query()
+      .where('project_id', params.projects_id)
+      .with('user')
+      .fetch()
+    return tasks
   }
 
   /**
    * Create/save a new task.
    * POST tasks
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, params }) {
+    const data = request.only([
+      'user_id',
+      'title',
+      'description',
+      'due_date',
+      'file_id'
+    ])
+    const task = await Task.create({ ...data, project_id: params.projects_id })
+    return task
   }
 
   /**
    * Display a single task.
    * GET tasks/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing task.
-   * GET tasks/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show({ params }) {
+    const task = await Task.findOrFail(params.id)
+    return task
   }
 
   /**
    * Update task details.
    * PUT or PATCH tasks/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params, request }) {
+    const task = await Task.findOrFail(params.id)
+    const data = request.only([
+      'user_id',
+      'title',
+      'description',
+      'due_date',
+      'file_id'
+    ])
+    task.merge(data)
+    await task.save()
+    return task
   }
 
   /**
    * Delete a task with id.
    * DELETE tasks/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params, request, response }) {
+    const task = await Task.findOrFail(params.id)
+    await task.delete()
   }
 }
 
