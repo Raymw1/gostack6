b5236c549f9858ff800f9f4c71ffe08d87e30301
Raymw1 <rayan.rmw@gmail.com>
2021-11-04 23:15:05 -0300

    GoNode: adonisJS - task and project models done

--- gonode/04-adonisjs/app/Models/Project.js
+++ gonode/04-adonisjs/app/Models/Project.js
@@ -3,6 +3,14 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Project extends Model {}
+class Project extends Model {
+  user() {
+    return this.belongsTo('App/Models/User')
+  }
+
+  tasks() {
+    return this.hasMany('App/Models/Task')
+  }
+}
 
 module.exports = Project
--- gonode/04-adonisjs/app/Models/Task.js
+++ gonode/04-adonisjs/app/Models/Task.js
@@ -3,6 +3,18 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class Task extends Model {}
+class Task extends Model {
+  user() {
+    return this.belongsTo('App/Models/User')
+  }
+
+  project() {
+    return this.belongsTo('App/Models/Project')
+  }
+
+  file() {
+    return this.belongsTo('App/Models/File')
+  }
+}
 
 module.exports = Task
