5a13cc6aa822a92b06e2814e08894ccd911c8e20
Raymw1 <rayan.rmw@gmail.com>
2021-11-08 19:26:46 -0300

    GoNode: adonisJS - validation: Project validators

--- gonode/04-adonisjs/start/routes.js
+++ gonode/04-adonisjs/start/routes.js
@@ -16,6 +16,8 @@ Route.get('/files/:id', 'FileController.show')
 
 Route.group(() => {
   Route.post('/files', 'FileController.store')
-  Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['projects.store'], ['Project']]]))
   Route.resource('projects.tasks', 'TaskController').apiOnly()
 }).middleware(['auth'])
