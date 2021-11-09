bfc7389df6629d41fb12103043bc2c326747c99d
Raymw1 <rayan.rmw@gmail.com>
2021-11-08 22:33:13 -0300

    GoNode: adonisJS - using transactions

--- gonode/04-adonisjs/app/Controllers/Http/UserController.js
+++ gonode/04-adonisjs/app/Controllers/Http/UserController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
@@ -7,8 +8,10 @@ class UserController {
     // const data = request.all()
     const data = request.only(['username', 'email', 'password'])
     const addresses = request.input('addresses')
-    const user = await User.create(data)
-    await user.addresses().createMany(addresses)
+    const trx = await Database.beginTransaction()
+    const user = await User.create(data, trx)
+    await user.addresses().createMany(addresses, trx)
+    await trx.commit() // FINALLY SAVE
     return user
   }
 }
