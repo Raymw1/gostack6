64224c6d8d8817dd6c603059fbe2cc5bdad3c75c
Raymw1 <rayan.rmw@gmail.com>
2021-11-09 21:58:06 -0300

    GoNode: challenge4 - update user

--- gonode/challenges/challenge4/app/Controllers/Http/UserController.js
+++ gonode/challenges/challenge4/app/Controllers/Http/UserController.js
@@ -1,12 +1,29 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Hash = use('Hash')
 
 class UserController {
   async store({ request }) {
     const data = request.only(['username', 'email', 'password'])
     const user = await User.create(data)
     return user
+  }
+
+  async update({ request, response }) {
+    try {
+      const { email, oldPassword } = request.all()
+      // await auth.attempt(email, oldPassword)
+      const user = await User.findByOrFail('email', email)
+      if (!(await Hash.verify(oldPassword, user.password)))
+        return response.status(401).send({ error: 'Invalid password' })
+      const data = request.only(['username', 'password'])
+      user.merge(data)
+      await user.save()
+      return user
+    } catch (error) {
+      return response.status(400).send({ error: error.message })
+    }
   }
 }
 
--- gonode/challenges/challenge4/start/routes.js
+++ gonode/challenges/challenge4/start/routes.js
@@ -18,3 +18,7 @@ const Route = use('Route')
 
 Route.post('/users', 'UserController.store')
 Route.post('/sessions', 'SessionController.store')
+
+Route.group(() => {
+  Route.put('/users', 'UserController.update')
+}).middleware(['auth'])
