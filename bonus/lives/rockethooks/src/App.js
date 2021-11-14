3cbee284fc7e43b2091ecee36a78b77be17de232
Raymw1 <rayan.rmw@gmail.com>
2021-11-13 22:18:53 -0300

    Extra: ReactHooks - useState

--- bonus/lives/rockethooks/src/App.js
+++ bonus/lives/rockethooks/src/App.js
@@ -1,7 +1,12 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class App extends Component {
-  render() {
-    return <h1>Hello, World!</h1>;
-  }
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  // ==== VALUE || CHANGE VALUE FUNCTION ====
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Add</button>
+    </>
+  );
 }
