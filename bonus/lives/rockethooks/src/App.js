7411403420eb99bdc5c365155b61c352b4b53448
Raymw1 <rayan.rmw@gmail.com>
2021-11-13 22:28:31 -0300

    Extra: ReactHooks - useReducer

--- bonus/lives/rockethooks/src/App.js
+++ bonus/lives/rockethooks/src/App.js
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
+
+function counter(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { counter: state.counter + 1 };
+    case "decrement":
+      return { counter: state.counter - 1 };
+    default:
+      return state;
+  }
+}
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(counter, { counter: 0 });
   // ==== VALUE || CHANGE VALUE FUNCTION ====
+
+  function increment() {
+    // setCounter(counter + 1);
+    dispatch({ type: "increment" });
+  }
+
+  function decrement() {
+    // setCounter(counter + 1);
+    dispatch({ type: "decrement" });
+  }
+
   return (
     <>
-      <h1>Counter: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>Add</button>
+      <h1>Counter: {state.counter}</h1>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
     </>
   );
 }
