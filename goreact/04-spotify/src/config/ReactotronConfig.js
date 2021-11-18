2f68fe0fe9b4175b08723c66d1f6492bf7b324ba
Raymw1 <rayan.rmw@gmail.com>
2021-11-17 21:46:37 -0300

    GoReact: spotify - reactotron | redux & sagas

--- goreact/04-spotify/src/config/ReactotronConfig.js
+++ goreact/04-spotify/src/config/ReactotronConfig.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === "development") {
     .use(sagaPlugin())
     .connect();
 
-  console.tron = tron;
-
   tron.clear();
+
+  console.tron = tron;
 }
--- goreact/04-spotify/src/store/index.js
+++ goreact/04-spotify/src/store/index.js
@@ -1,14 +1,16 @@
 import { createStore, compose, applyMiddleware } from "redux";
-import createSagaMiddleware from "@redux-saga/core";
+import createSagaMiddleware from "redux-saga";
 
 import reducers from "./ducks";
 import sagas from "./sagas";
 
-const sagaMonitor = process.env.NODE_ENV
-  ? console.tron.createSagaMonitor()
-  : null;
+const middlewares = [];
+const sagaMonitor =
+  process.env.NODE_ENV === "development"
+    ? console.tron.createSagaMonitor()
+    : null;
 const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
-const middlewares = [sagaMiddleware];
+middlewares.push(sagaMiddleware);
 
 const composer =
   process.env.NODE_ENV === "development"
