57ccfc95ecd1a597c319469e4bbc3c0e07c22d02
Raymw1 <rayan.rmw@gmail.com>
2021-11-18 21:16:03 -0300

    GoReact: spotify - playlistDetails: redux & saga done

--- goreact/04-spotify/src/store/sagas/index.js
+++ goreact/04-spotify/src/store/sagas/index.js
@@ -1,8 +1,14 @@
 import { all, takeLatest } from "redux-saga/effects";
 
-import { Types as PlaylistTypes } from "../ducks/playlists";
+import { Types as PlaylistsTypes } from "../ducks/playlists";
 import { getPlaylists } from "./playlists";
 
+import { Types as PlaylistDetailsTypes } from "../ducks/playlistDetails";
+import { getPlaylistDetails } from "./playlistDetails";
+
 export default function* rootSaga() {
-  yield all([takeLatest(PlaylistTypes.GET_REQUEST, getPlaylists)]);
+  yield all([
+    takeLatest(PlaylistsTypes.GET_REQUEST, getPlaylists),
+    takeLatest(PlaylistDetailsTypes.GET_REQUEST, getPlaylistDetails),
+  ]);
 }
