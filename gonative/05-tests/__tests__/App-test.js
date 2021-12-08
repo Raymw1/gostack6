503d65570cf22e5745295a4d65423cf8934d91e4
Raymw1 <rayan.rmw@gmail.com>
2021-12-08 16:48:00 -0300

    GoNative: tests - First test

--- gonative/05-tests/__tests__/App-test.js
+++ gonative/05-tests/__tests__/App-test.js
@@ -1,14 +1,8 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('View').exists()).toBe(true);
 });
