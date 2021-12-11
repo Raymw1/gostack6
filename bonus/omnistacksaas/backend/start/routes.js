252c6513efb6b1344b16a0efaba8b459e46d5ec8
Raymw1 <rayan.rmw@gmail.com>
2021-12-11 18:15:26 -0300

    Bonus: OmnistackSaaS - Backend: User registration

--- bonus/omnistacksaas/backend/start/routes.js
+++ bonus/omnistacksaas/backend/start/routes.js
@@ -16,6 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
+Route.post('/users', 'UserController.store')
 Route.post('/sessions', 'SessionController.store')
 
 Route.group(() => {
