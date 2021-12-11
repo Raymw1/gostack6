e75ee9671bda561b3aaa7fedbf2cf9f8362024ad
Raymw1 <rayan.rmw@gmail.com>
2021-12-11 19:51:38 -0300

    Bonus: OmnistackSaaS - Backend: Applying Validators to routes

--- bonus/omnistacksaas/backend/start/routes.js
+++ bonus/omnistacksaas/backend/start/routes.js
@@ -16,14 +16,18 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.post('/users', 'UserController.store')
-Route.post('/sessions', 'SessionController.store')
+Route.post('/users', 'UserController.store').validator('User')
+Route.post('/sessions', 'SessionController.store').validator('Session')
 
 Route.group(() => {
-  Route.resource('/teams', 'TeamController').apiOnly()
+  Route.resource('/teams', 'TeamController')
+    .apiOnly()
+    .validator(new Map([[['teams.store', 'teams.update'], ['Team']]]))
 }).middleware(['auth'])
 
 Route.group(() => {
-  Route.post('/invites', 'InviteController.store')
-  Route.resource('/projects', 'ProjectController').apiOnly()
+  Route.post('/invites', 'InviteController.store').validator('Invite')
+  Route.resource('/projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['projects.store', 'projects.update'], ['Project']]]))
 }).middleware(['auth', 'team'])
