1ddbd2cdd4c61fb1f18b76c7925b60b5ee535c06
Raymw1 <rayan.rmw@gmail.com>
2021-12-11 17:59:35 -0300

    Bonus: OmnistackSaaS - Backend: Emails with Jobs

--- bonus/omnistacksaas/backend/app/Models/Hooks/InviteHook.js
+++ bonus/omnistacksaas/backend/app/Models/Hooks/InviteHook.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const User = use('App/Models/User')
+const Kue = use('Kue')
+const Job = use('App/Jobs/InvitationEmail')
 
 const InviteHook = (exports = module.exports = {})
 
@@ -10,6 +12,8 @@ InviteHook.sendInvitationEmail = async (invite) => {
   if (invited) {
     await invited.teams().attach(team_id)
   } else {
-    console.log('CREATE ACCOUNT')
+    const user = await invite.user().fetch()
+    const team = await invite.team().fetch()
+    Kue.dispatch(Job.key, { user, team, email }, { attempts: 3 })
   }
 }
