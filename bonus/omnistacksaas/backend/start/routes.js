d011b4757d142783e594e531006d972627550b59
Raymw1 <rayan.rmw@gmail.com>
2021-12-10 21:32:41 -0300

    Bonus: OmnistackSaaS - Backend: Authentication done

--- bonus/omnistacksaas/backend/start/routes.js
+++ bonus/omnistacksaas/backend/start/routes.js
@@ -16,6 +16,8 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
+Route.post('/sessions', 'SessionController.store')
+
 Route.get('/', () => {
   return { greeting: 'Hello world in JSON' }
 })
