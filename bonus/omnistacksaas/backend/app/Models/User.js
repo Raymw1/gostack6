a1196ac8cb865b012a208718c13edfe9301d5e7d
Raymw1 <rayan.rmw@gmail.com>
2021-12-11 21:34:48 -0300

    Bonus: OmnistackSaaS - Backend: Authorization setup

--- bonus/omnistacksaas/backend/app/Models/User.js
+++ bonus/omnistacksaas/backend/app/Models/User.js
@@ -21,6 +21,10 @@ class User extends Model {
     })
   }
 
+  teamJoins() {
+    return this.hasMany('App/Models/UserTeam')
+  }
+
   tokens() {
     return this.hasMany('App/Models/Token')
   }
@@ -29,6 +33,27 @@ class User extends Model {
     return this.belongsToMany('App/Models/Team').pivotModel(
       'App/Models/UserTeam'
     )
+  }
+
+  async is(expression) {
+    const team = await this.teamJoins()
+      .where('team_id', this.currentTeam)
+      .first()
+    return team.is(expression)
+  }
+
+  async can(expression) {
+    const team = await this.teamJoins()
+      .where('team_id', this.currentTeam)
+      .first()
+    return team.can(expression)
+  }
+
+  async scope(required) {
+    const team = await this.teamJoins()
+      .where('team_id', this.currentTeam)
+      .first()
+    return team.scope(required)
   }
 }
 
--- bonus/omnistacksaas/backend/database/seeds/DatabaseSeeder.js
+++ bonus/omnistacksaas/backend/database/seeds/DatabaseSeeder.js
@@ -2,6 +2,9 @@
 
 const User = use('App/Models/User')
 
+const Role = use('Adonis/Acl/Role')
+const Permission = use('Adonis/Acl/Permission')
+
 class DatabaseSeeder {
   async run() {
     const user = await User.create({
@@ -9,10 +12,42 @@ class DatabaseSeeder {
       email: 'rayan@rocketseat.com.br',
       password: '123456'
     })
-    await user.teams().create({
+
+    const createInvite = await Permission.create({
+      slug: 'invites_create',
+      name: 'Invite members'
+    })
+
+    const createProject = await Permission.create({
+      slug: 'projects_create',
+      name: 'Create projects'
+    })
+
+    const admin = await Role.create({
+      slug: 'administrator',
+      name: 'Administrator'
+    })
+
+    const moderator = await Role.create({
+      slug: 'moderator',
+      name: 'Moderator'
+    })
+
+    await Role.create({
+      slug: 'visitor',
+      name: 'Visitor'
+    })
+
+    await admin.permissions().attach([createInvite.id, createProject.id])
+    await moderator.permissions().attach([createProject.id])
+
+    const team = await user.teams().create({
       name: 'Rocketseat',
       user_id: user.id
     })
+
+    const teamJoin = await user.teamJoins().where('team_id', team.id).first()
+    await teamJoin.roles().attach([admin.id])
   }
 }
 
