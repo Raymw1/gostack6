e58ee77c9e67f5ed1854f81790c394df32b061e3
Raymw1 <rayan.rmw@gmail.com>
2021-12-23 22:23:01 -0300

    Bonus: OmnistackSaaS - Mobile: Login structure

--- bonus/omnistacksaas/mobile/src/pages/SignIn/index.js
+++ bonus/omnistacksaas/mobile/src/pages/SignIn/index.js
@@ -1,14 +1,66 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 
 import styles from './styles';
 
 export default class SingIn extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleSubmit = () => {
+    const {email, password} = this.state;
+  };
+
   render() {
+    const {email, password} = this.state;
     return (
-      <View style={styles.container}>
-        <Text> SignIn </Text>
-      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : null}
+        style={styles.container}>
+        <View>
+          <Text style={styles.title}>SignIn</Text>
+          <Text style={styles.label}>E-MAIL</Text>
+          <TextInput
+            style={styles.input}
+            value={email}
+            onChangeText={text => this.setState({email: text})}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            underlineColorAndroid="transparent"
+            autoFocus
+            returnKeyType="next"
+            onSubmitEditing={() => this.passordInput.focus()}
+          />
+          <Text style={styles.label}>PASSWORD</Text>
+          <TextInput
+            style={styles.input}
+            value={password}
+            onChangeText={text => this.setState({password: text})}
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
+            underlineColorAndroid="transparent"
+            returnKeyType="send"
+            ref={el => {
+              this.passordInput = el;
+            }}
+            onSubmitEditing={this.handleSubmit}
+          />
+          <TouchableOpacity onPress={this.handleSubmit} style={styles.button}>
+            <Text style={styles.buttonText}>SignIn</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
     );
   }
 }
