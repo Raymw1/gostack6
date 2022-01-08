e79f08f6c6320c5748c6d219e5fdafaf163a07da
Raymw1 <rayan.rmw@gmail.com>
2022-01-08 15:40:30 -0300

    FinalProject: Backend - Exception Handling

--- finalchallenge/backend/src/routes.js
+++ finalchallenge/backend/src/routes.js
@@ -1,4 +1,7 @@
 const express = require("express");
+// const handle = require("express-async-handler");
 const routes = express.Router();
+
+// routes.post("/test", validate(), handle(controllers.test.store))
 
 module.exports = routes;
--- finalchallenge/backend/src/server.js
+++ finalchallenge/backend/src/server.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-// const validate = require("express-validation");
-// const Youch = require("youch");
+const validate = require("express-validation");
+const Youch = require("youch");
 // const Sentry = require("@sentry/node");
 // const databaseConfig
 // const sentryConfig
@@ -14,6 +14,7 @@ class App {
     this.isDev = process.env.NODE_ENV !== "production";
     this.middlewares();
     this.routes();
+    this.exception();
   }
 
   middlewares() {
@@ -24,6 +25,23 @@ class App {
 
   routes() {
     this.express.use(require("./routes"));
+  }
+
+  exception() {
+    this.express.use(async (err, req, res, next) => {
+      if (err instanceof validate.ValidationError) {
+        return res.status(err.statusCode).json(err);
+      }
+
+      if (process.env.NODE_ENV !== "production") {
+        const youch = new Youch(err);
+        return res.json(await youch.toJSON());
+      }
+
+      return res
+        .status(err.status || 500)
+        .json({ error: "Internal Server Error" });
+    });
   }
 }
 
