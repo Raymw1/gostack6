d38de5d77b921f0e946d117ecbbd6754a47beabd
Raymw1 <rayan.rmw@gmail.com>
2022-01-08 15:57:45 -0300

    FinalProject: Backend - Configuring Sequelize

---
+++ finalchallenge/backend/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: process.env.DATABASE_TYPE,
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
