2d49f37c87b7ef519e0b653e22d291939d0cc505
Raymw1 <rayan.rmw@gmail.com>
2022-01-10 20:09:35 -0300

    FinalProject: Backend - TDD: Create User - Basics

--- finalchallenge/backend/__tests__/integration/user.create.test.js
+++ finalchallenge/backend/__tests__/integration/user.create.test.js
@@ -1,12 +1,11 @@
 const request = require("supertest");
 
 const app = require("../../src/server");
-// const { User } = require("../../src/app/models");
 
 describe("Create User", () => {
   it("should be able to create user with valid credentials", async () => {
     // POST /users { name, email, password }
-    const response = request(app).post("/users").send({
+    const response = await request(app).post("/users").send({
       name: "Rayan",
       email: "rayan@rocketseat.com",
       password_hash: "123123",
--- finalchallenge/backend/src/routes.js
+++ finalchallenge/backend/src/routes.js
@@ -1,8 +1,10 @@
 const routes = require("express").Router();
-// const handle = require("express-async-handler");
+const handle = require("express-async-handler");
+
+const UserController = require("./app/controllers/UserController");
 
 // routes.post("/test", validate(), handle(controllers.test.store))
 
-routes.get("/", (req, res) => res.send("Hello, World!"));
+routes.post("/users", handle(UserController.store));
 
 module.exports = routes;
