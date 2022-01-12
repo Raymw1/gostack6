cfe1d2b2d0e23bdee0b8606e8e958151935b39f3
Raymw1 <rayan.rmw@gmail.com>
2022-01-12 20:52:08 -0300

    FinalProject: Backend - TDD: Sessions - Validation

--- finalchallenge/backend/src/app/validators/Session.js
+++ finalchallenge/backend/src/app/validators/Session.js
@@ -2,7 +2,6 @@ const Joi = require("joi");
 
 module.exports = {
   body: Joi.object({
-    name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
